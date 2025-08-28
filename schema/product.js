dee63d3208422eab0a45b7df912715c14cc418b9
fairuz akhdan <fairuzakhdan01@gmail.com>
2025-08-28 14:51:13 +0700

    first commit

--- schema/product.js
+++ schema/product.js
@@ -4,7 +4,7 @@ const productSchema = Joi.object({
     product: Joi.object({
         Name: Joi.string().required().min(1),
         Price: Joi.string().required().min(1),
-        Stock: Joi.number().required().min(1),
+        Stock: Joi.number().required().min(1).integer(),
         Deskripsi: Joi.string().required().min(1),
     })
 })
